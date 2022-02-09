918e2b9219789098a919716a4a8c2319ba7432ad
adryel97 <adryel.giovaniny@gmail.com>
2022-02-09 17:27:00 -0300

    banco de dados

--- db_crm.sql
+++ db_crm.sql
@@ -45,11 +45,11 @@ CREATE TABLE `tbl_picture` (
   `name_picture` varchar(50) DEFAULT NULL,
   `date_picture` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id_picture`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tbl_picture` */
 
-insert  into `tbl_picture`(`id_picture`,`fk_user`,`name_picture`,`date_picture`) values (1,1,'a','2021-08-31 11:04:18'),(2,1,'asdas','2021-08-31 11:10:07'),(3,1,'asdas','2021-08-31 11:10:20');
+insert  into `tbl_picture`(`id_picture`,`fk_user`,`name_picture`,`date_picture`) values (21,2,'Administração','2021-09-17 08:43:47'),(27,1,'Novo Quadro','2021-09-17 12:01:08'),(36,1,'Meu novo quadro','2022-02-09 16:52:14'),(37,1,'Quadro admin','2022-02-09 16:57:54');
 
 /*Table structure for table `tbl_status` */
 
@@ -63,11 +63,11 @@ CREATE TABLE `tbl_status` (
   `color_status` varchar(50) DEFAULT NULL,
   `date_status` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id_status`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tbl_status` */
 
-insert  into `tbl_status`(`id_status`,`fk_picture`,`fk_user`,`name_status`,`color_status`,`date_status`) values (1,3,1,'Novo titulo','purple','2021-08-31 15:11:51'),(14,3,1,'a','primary','2021-09-01 09:43:01'),(15,3,1,'meu deus','cyan','2021-09-01 10:11:07'),(16,3,1,'oloko','red','2021-09-01 10:13:00');
+insert  into `tbl_status`(`id_status`,`fk_picture`,`fk_user`,`name_status`,`color_status`,`date_status`) values (1,3,1,'Novo titulo','purple','2021-08-31 15:11:51'),(14,3,1,'a','primary','2021-09-01 09:43:01'),(15,3,1,'meu deus','cyan','2021-09-01 10:11:07'),(16,3,1,'oloko','red','2021-09-01 10:13:00'),(21,21,2,'Status 1','primary','2021-09-17 08:45:36'),(24,21,2,'Status 2','pink','2021-09-17 09:51:55'),(26,21,2,'Status 4','yellow','2021-09-17 09:52:34'),(27,21,2,'Status 5 ','green','2021-09-17 09:52:42'),(28,27,1,'Status 1','primary','2021-09-17 12:01:18'),(29,27,1,'Status 2','orange','2021-09-17 12:01:23'),(30,27,1,'Status 3','cyan','2021-09-17 12:01:29');
 
 /*Table structure for table `tbl_task` */
 
@@ -82,12 +82,13 @@ CREATE TABLE `tbl_task` (
   `account_task` mediumtext DEFAULT NULL,
   `date_task` timestamp NULL DEFAULT current_timestamp(),
   `order_task` int(11) DEFAULT NULL,
+  `fk_user_received` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_task`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tbl_task` */
 
-insert  into `tbl_task`(`id_task`,`fk_status`,`fk_picture`,`fk_user`,`name_task`,`account_task`,`date_task`,`order_task`) values (1,1,3,1,'tarefa 1','tarefa 1','2021-09-01 11:10:46',1),(2,1,3,1,'New tarefa','','2021-09-01 11:45:27',0),(3,14,3,1,'aaa','sadasdsa','2021-09-01 11:55:28',0),(4,1,3,1,'aaa','sadasdsa','2021-09-01 11:55:29',0),(8,14,3,1,'Oloko meu','','2021-09-01 14:02:47',NULL);
+insert  into `tbl_task`(`id_task`,`fk_status`,`fk_picture`,`fk_user`,`name_task`,`account_task`,`date_task`,`order_task`,`fk_user_received`) values (37,30,27,1,'Tarefa Nova','Fazer tal coisa.','2022-01-10 10:28:18',0,1),(38,28,27,1,'tarefa 3','testando','2022-02-09 10:08:48',1,1),(39,29,27,1,'teste 4','Editado o texto aaaaa','2022-02-09 10:09:03',0,2),(41,28,27,1,'tarefa 4','dsadsadsa','2022-02-09 14:08:06',0,1);
 
 /*Table structure for table `tbl_user` */
 
