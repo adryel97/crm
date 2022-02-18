972c7843ce73e8f5d0e8748ab8c2da12df11976b
adryel97 <adryel.giovaniny@gmail.com>
2022-02-17 23:58:53 -0300

    -

--- db_crm.sql
+++ db_crm.sql
@@ -1,5 +1,5 @@
 /*
-SQLyog Ultimate v12.09 (64 bit)
+SQLyog Professional v12.09 (64 bit)
 MySQL - 10.4.10-MariaDB : Database - db_crm
 *********************************************************************
 */
@@ -35,6 +35,10 @@ CREATE TABLE `tbl_history_protect` (
 
 /*Data for the table `tbl_history_protect` */
 
+LOCK TABLES `tbl_history_protect` WRITE;
+
+UNLOCK TABLES;
+
 /*Table structure for table `tbl_picture` */
 
 DROP TABLE IF EXISTS `tbl_picture`;
@@ -49,7 +53,11 @@ CREATE TABLE `tbl_picture` (
 
 /*Data for the table `tbl_picture` */
 
+LOCK TABLES `tbl_picture` WRITE;
+
 insert  into `tbl_picture`(`id_picture`,`fk_user`,`name_picture`,`date_picture`) values (21,2,'Administração','2021-09-17 08:43:47'),(27,1,'Novo Quadro','2021-09-17 12:01:08'),(36,1,'Meu novo quadro','2022-02-09 16:52:14'),(37,1,'Quadro admin','2022-02-09 16:57:54');
+
+UNLOCK TABLES;
 
 /*Table structure for table `tbl_status` */
 
@@ -67,7 +75,11 @@ CREATE TABLE `tbl_status` (
 
 /*Data for the table `tbl_status` */
 
+LOCK TABLES `tbl_status` WRITE;
+
 insert  into `tbl_status`(`id_status`,`fk_picture`,`fk_user`,`name_status`,`color_status`,`date_status`) values (1,3,1,'Novo titulo','purple','2021-08-31 15:11:51'),(14,3,1,'a','primary','2021-09-01 09:43:01'),(15,3,1,'meu deus','cyan','2021-09-01 10:11:07'),(16,3,1,'oloko','red','2021-09-01 10:13:00'),(21,21,2,'Status 1','primary','2021-09-17 08:45:36'),(24,21,2,'Status 2','pink','2021-09-17 09:51:55'),(26,21,2,'Status 4','yellow','2021-09-17 09:52:34'),(27,21,2,'Status 5 ','green','2021-09-17 09:52:42'),(28,27,1,'Status 1','primary','2021-09-17 12:01:18'),(29,27,1,'Status 2','orange','2021-09-17 12:01:23'),(30,27,1,'Status 3','cyan','2021-09-17 12:01:29');
+
+UNLOCK TABLES;
 
 /*Table structure for table `tbl_task` */
 
@@ -84,11 +96,39 @@ CREATE TABLE `tbl_task` (
   `order_task` int(11) DEFAULT NULL,
   `fk_user_received` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_task`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tbl_task` */
 
-insert  into `tbl_task`(`id_task`,`fk_status`,`fk_picture`,`fk_user`,`name_task`,`account_task`,`date_task`,`order_task`,`fk_user_received`) values (37,30,27,1,'Tarefa Nova','Fazer tal coisa.','2022-01-10 10:28:18',0,1),(38,28,27,1,'tarefa 3','testando','2022-02-09 10:08:48',1,1),(39,29,27,1,'teste 4','Editado o texto aaaaa','2022-02-09 10:09:03',0,2),(41,28,27,1,'tarefa 4','dsadsadsa','2022-02-09 14:08:06',0,1);
+LOCK TABLES `tbl_task` WRITE;
+
+insert  into `tbl_task`(`id_task`,`fk_status`,`fk_picture`,`fk_user`,`name_task`,`account_task`,`date_task`,`order_task`,`fk_user_received`) values (46,28,27,1,'Tarefa 1','teste','2022-02-16 20:25:34',0,1);
+
+UNLOCK TABLES;
+
+/*Table structure for table `tbl_task_history` */
+
+DROP TABLE IF EXISTS `tbl_task_history`;
+
+CREATE TABLE `tbl_task_history` (
+  `id_task` int(11) DEFAULT NULL,
+  `fk_status` int(11) DEFAULT NULL,
+  `fk_picture` int(11) DEFAULT NULL,
+  `fk_user` int(11) DEFAULT NULL,
+  `name_task` varchar(50) DEFAULT NULL,
+  `account_task` mediumtext DEFAULT NULL,
+  `date_task` timestamp NULL DEFAULT current_timestamp(),
+  `order_task` int(11) DEFAULT NULL,
+  `fk_user_received` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `tbl_task_history` */
+
+LOCK TABLES `tbl_task_history` WRITE;
+
+insert  into `tbl_task_history`(`id_task`,`fk_status`,`fk_picture`,`fk_user`,`name_task`,`account_task`,`date_task`,`order_task`,`fk_user_received`) values (47,29,27,1,'Tarefa 2','asa','2022-02-16 20:32:34',0,1);
+
+UNLOCK TABLES;
 
 /*Table structure for table `tbl_user` */
 
@@ -103,11 +143,15 @@ CREATE TABLE `tbl_user` (
   `birth_date_user` date DEFAULT NULL,
   `date_register_user` timestamp NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tbl_user` */
 
-insert  into `tbl_user`(`id_user`,`name_user`,`email_user`,`password_user`,`accept_user`,`birth_date_user`,`date_register_user`) values (1,'Pedroso','pedrinho_foda@gmail.com','81dc9bdb52d04dc20036dbd8313ed055','true','2000-02-20','2021-08-30 15:19:01'),(2,'Tonhão','toinho@gmail.com','81dc9bdb52d04dc20036dbd8313ed055','true','2000-02-20','2021-08-31 10:00:23');
+LOCK TABLES `tbl_user` WRITE;
+
+insert  into `tbl_user`(`id_user`,`name_user`,`email_user`,`password_user`,`accept_user`,`birth_date_user`,`date_register_user`) values (1,'Pedroso','pedrinho_foda@gmail.com','81dc9bdb52d04dc20036dbd8313ed055','true','2000-02-20','2021-08-30 15:19:01'),(2,'Tonhão','toinho@gmail.com','81dc9bdb52d04dc20036dbd8313ed055','true','2000-02-20','2021-08-31 10:00:23'),(3,'adryel giovaniny dos santos','adry_gio97@hotmail.com','e10adc3949ba59abbe56e057f20f883e','true','1997-08-06','2022-02-16 19:49:33');
+
+UNLOCK TABLES;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
